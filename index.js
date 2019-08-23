a7c60e889837b0881d61286510a4381959572a13
Silas Groskreutz <silasgroskreutz@gmail.com>
2019-08-23 16:55:46 -0500

    redid sections and changed buttons / behavior

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $('.js-home').click(function (e) {
     e.preventDefault();
     $('.home').show();
     $('.portfolio').hide();
-    $('.about').hide();
+    $('.contact').hide();
 	/*console.log("Clicked!"); */
 });
 
@@ -10,7 +10,7 @@ $('.js-portfolio').click(function (e) {
     e.preventDefault();
     $('.home').hide();
     $('.portfolio').show();
-    $('.about').hide();
+    $('.contact').hide();
 	/*console.log("Clicked!"); */
 });
 
@@ -18,12 +18,6 @@ $('.js-about').click(function (e) {
     e.preventDefault();
     $('.home').hide();
     $('.portfolio').hide();
-    $('.about').show();
+    $('.contact').show();
 	/*console.log("Clicked!"); */
 });
-
-/*  
-May implement something later with picture of dogs, was not in original plans but was suggested
-$('.js-dogs').click(function (e) {
-    $('.js-dogs').toggle();
-}); */
